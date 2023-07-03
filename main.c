eb36d2758e583d38d381b65cc57e7e962f3a956b
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-07-03 17:50:49 +0000

    added options for suv and truck vehicles

--- main.c
+++ main.c
@@ -37,42 +37,72 @@ int main()
 	/* If statements to calculate consumption for different vehicle class */
 	if (strcmp(vehicle, "A") == 0 || strcmp(vehicle, "SALOON") == 0)
 	{
-		if (strcmp(fuel, "PETROL") == 0)
+		if (strcmp(fuel, "PETROL") == 0 || strcmp(fuel, "A") == 0)
 		{
 			price = 200.00;
 			average = 10.00;
 			ttl_amnt = ((distance / average) * price);
 			printf("A safari of %d km using a %s will consume fuel totalling Ksh.%.2f\n", distance, vehicle, ttl_amnt);
 		}
-		else if (strcmp(fuel, "DIESEL") == 0)
+		else if (strcmp(fuel, "DIESEL") == 0 || strcmp(fuel, "B") == 0)
 		{
-			price = 150.00;
-			average = 8.00;
+			price = 180.00;
+			average = 10.00;
 			ttl_amnt = ((distance / average) * price);
-			printf("A safari of %d km using a %s will consume fuel totalling Ksh.%.2f\n", distance, vehicle, ttl_amnt);
+			printf("A safari of %d km using %s will consume fuel totalling Ksh.%.2f\n", distance, vehicle, ttl_amnt);
 		}
 		else
 		{
-			printf("Enter a valid type of fuel from the given choices");
+			printf("Error!\n");
 		}
 
 	}
+	else if (strcmp(vehicle, "B") == 0 || strcmp(vehicle, "SUV") == 0)
+	{
+		if (strcmp(fuel, "PETROL") == 0 || strcmp(fuel, "A") == 0)
+		{
+			price = 200.00;
+			average = 6.00;
+			ttl_amnt = ((distance / average) * price);
+			printf("A safari of %d km using %s will consume fuel totalling Ksh.%.2f\n", distance, vehicle, ttl_amnt);
+		}
+		else if (strcmp(fuel, "DIESEL") == 0 || strcmp(fuel, "B") == 0)
+		{
+			price = 180.00;
+			average = 6.00;
+			ttl_amnt = ((distance / average) * price);
+			printf("A safari of %d km using %s will consume fuel totalling Ksh.%.2f\n", distance, vehicle, ttl_amnt);
+		}
+		else
+		{
+			printf("Error!\n");
+		}
+	}
+	else if (strcmp(vehicle, "C") == 0 || strcmp(vehicle, "TRUCK") == 0)
+	{
+		if (strcmp(fuel, "PETROL") == 0 || strcmp(fuel, "A") == 0)
+		{
+			price = 200.00;
+			average = 5.00;
+			ttl_amnt = ((distance / average) * price);
+			printf("A safari of %d km using %s will consume fuel totalling Ksh.%.2f\n", distance, vehicle, ttl_amnt);
+		}
+		else if (strcmp(fuel, "DIESEL") == 0 || strcmp(fuel, "B") == 0)
+		{
+			price = 180.00;
+			average = 5.00;
+			ttl_amnt = ((distance / average) * price);
+			printf("A safari of %d km using %s will consume fuel totalling Ksh.%.2f\n", distance, vehicle, ttl_amnt);
+		}
+		else
+		{
+			printf("Error!\n");
+		}
+	}
+	else
+	{
+		printf("Error!\n");
+	}
 
-
-
-	// printf("Price per liter of the fuel to be used: ");
-	//scanf("%f", &price);		/* price in shillings */
-
-	// printf("Enter the average consumption of the vehicle to used: ");
-	//scanf("%f", &average);		/* average is in liter per kilometer */
-
-	/* calculate the liters required */
-	//liters = distance / average;
-	
-	/* calculate the total amount to be spent */
-	//total_amount = liters * price;
-
-	/* output the necessary information */
-	//printf("The expected distance to be travelled is %d.\nYou are expected to consume approximately %.2f liters.\nThe expected total amount to be spent is %.2fKsh.\n", distance, liters, total_amount);
 	return (0);
 }
