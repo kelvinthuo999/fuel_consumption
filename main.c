7df572097db6d1d639c7f8c3608e2196fc3314f0
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-11-07 10:05:57 +0300

    added welcome message

--- main.c
+++ main.c
@@ -11,8 +11,11 @@ int main(void)
 	char veh_type[20];
 	/*char fuel_type[20];*/
 
-	printf("\033[1;36m \t\t\ttFuel Consumption\033[0m\n");
-	getFlt("Enter the distance to be traveled", &distance);
+	printf("\033[1;36m \t\t\tFuel Consumption\033[0m\n");
+	printf("Welcome to the Fuel Consumption program.\nThis program helps you determine the approximate fuel you will need for your travel.");
+	printf("\nAre you a guest or user?");
+	printf("\n1. Guest \n2. User");
+	getFlt("\nEnter the distance to be traveled", &distance);
 	printf("The distance to be traveled is %.2f.\n", distance);
 
 	getStr("Enter string input", veh_type, 20);
